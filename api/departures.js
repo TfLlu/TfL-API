2563c13efad5a32a64fdb9a3148cef86ea772c2d
Daniel Duton <daniel@duton.lu>
2016-04-09 01:42:31 +0200

    use id for departures query

--- api/departures.js
+++ api/departures.js
@@ -1,5 +1,4 @@
 var request = require('request'),
-    stations = require('./stations.js'),
     Q = require('q');
 
 var departuresUrl = 'http://travelplanner.mobiliteit.lu/restproxy/departureBoard'
@@ -13,47 +12,46 @@ module.exports = {
         var result = [];
         var deferred = Q.defer();
 
-        stations.get(id).then(function(station) {
-            var specialId = 'A=1@O=' + station.name;
-            request(departuresUrl + encodeURI(specialId), function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var departures = JSON.parse(body).Departure;
-                    if (departures) {
-                        for (var i = 0; i < departures.length; i++) {
-                            var departure = {};
-                            switch (departures[i].Product.catCode) {
-                                case '2':
-                                departure.type = 'train';
-                                break;
-                                case '5':
-                                departure.type = 'bus';
-                                break;
-                                default:
-                                departure.type = 'unknown';
-                                break;
-                            }
-                            departure.line = departures[i].Product.line.trim();
-                            departure.number = parseInt(departures[i].Product.num.trim(), 10);
-
-                            var time = Math.round(Date.parse(departures[i].date + ' ' + departures[i].time) / 1000);
-                            if (departures[i].rtDate) {
-                                var realTime = Math.round(Date.parse(departures[i].rtDate + ' ' + departures[i].rtTime) / 1000);
-                                departure.departure = realTime;
-                                departure.delay = realTime - time;
-                            } else {
-                                departure.departure = time;
-                                departure.delay = 0;
-                            }
-                            departure.destination = departures[i].direction;
-                            result.push(departure);
+        var specialId = 'A=1@L=' + id;
+        request(departuresUrl + encodeURI(specialId), function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var departures = JSON.parse(body).Departure;
+                if (departures) {
+                    for (var i = 0; i < departures.length; i++) {
+                        var departure = {};
+                        switch (departures[i].Product.catCode) {
+                            case '2':
+                            departure.type = 'train';
+                            break;
+                            case '5':
+                            departure.type = 'bus';
+                            break;
+                            default:
+                            departure.type = 'unknown';
+                            break;
                         }
+                        departure.line = departures[i].Product.line.trim();
+                        departure.number = parseInt(departures[i].Product.num.trim(), 10);
+
+                        var time = Math.round(Date.parse(departures[i].date + ' ' + departures[i].time) / 1000);
+                        if (departures[i].rtDate) {
+                            var realTime = Math.round(Date.parse(departures[i].rtDate + ' ' + departures[i].rtTime) / 1000);
+                            departure.departure = realTime;
+                            departure.delay = realTime - time;
+                        } else {
+                            departure.departure = time;
+                            departure.delay = 0;
+                        }
+                        departure.destination = departures[i].direction;
+                        result.push(departure);
                     }
-                    deferred.resolve(result);
-                } else {
-                    deferred.reject(new Error(error));
                 }
-            });
+                deferred.resolve(result);
+            } else {
+                deferred.reject(new Error(error));
+            }
         });
+
         return deferred.promise;
     }
 
