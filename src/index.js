6fd120ad1c8187b057887e2589e148e20b90a2e9
Daniel Duton <daniel@duton.lu>
2016-12-02 14:27:46 +0100

    syntax

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Koa       from 'koa';
-import KoaRouter from 'koa-router';
+import Koa        from 'koa';
+import KoaRouter  from 'koa-router';
 import controller from './controller';
 
 const app    = new Koa();
