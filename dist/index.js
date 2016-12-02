997369ad10868f2f3d54a139546a834a84cb65a3
Daniel Duton <daniel@duton.lu>
2016-12-02 14:13:02 +0100

    add routes

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,19 @@ var _koa = require('koa');
 
 var _koa2 = _interopRequireDefault(_koa);
 
+var _koaRouter = require('koa-router');
+
+var _koaRouter2 = _interopRequireDefault(_koaRouter);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = new _koa2.default();
+const router = new _koaRouter2.default();
+
+router.get('/', ctx => {
+   ctx.body = '<h1>TFL API</h1>';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(9000);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import Koa from 'koa';
+import Koa       from 'koa';
+import KoaRouter from 'koa-router';
 
-const app = new Koa();
+const app    = new Koa();
+const router = new KoaRouter();
+
+router.get('/', ctx => {
+    ctx.body = '<h1>TFL API</h1>';
+});
+
+app.use(router.routes())
+   .use(router.allowedMethods());
+
+app.listen(9000);
