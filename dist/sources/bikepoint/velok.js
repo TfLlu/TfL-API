ed7018eb1c1d886a6153fa039f9aaad4c028c000
Thierry Degeling <thierry@syn2cat.lu>
2016-12-12 11:57:37 +0100

    compiler now handles bike types

--- dist/sources/bikepoint/velok.js
+++ dist/sources/bikepoint/velok.js
@@ -17,7 +17,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-const getRaw = () => (0, _requestPromiseNative2.default)('https://webservice.velok.lu/station.aspx');
+const getRaw = () => (0, _requestPromiseNative2.default)('https://webservice.velok.lu/stationattache.aspx');
 
 const get = exports.get = (() => {
     var _ref = _asyncToGenerator(function* () {
@@ -44,5 +44,54 @@ const stations = exports.stations = (() => {
 })();
 
 const compileStation = exports.compileStation = item => {
-    return item;
+
+    var dock_status = [];
+    var attache, status, bikeType;
+
+    for (var i = 1; i <= item.attaches; i++) {
+
+        attache = parseInt(item['attache' + i]);
+
+        switch (attache) {
+            case 0:
+                status = 'free';
+                bikeType = null;
+                break;
+
+            case 1:
+                status = 'occupied';
+                bikeType = 'manual';
+                break;
+
+            case 2:
+                status = 'occupied';
+                bikeType = 'electric';
+                break;
+        }
+
+        dock_status.push({
+            status: status,
+            bikeType: bikeType
+        });
+    }
+
+    return {
+        id: parseInt(item.nstation),
+        open: item.active == 1,
+        name: item.nom,
+        position: {
+            longitude: parseFloat(item.latitude),
+            latitude: parseFloat(item.longitude)
+        },
+        city: item.nomlocalite,
+        address: item.lieu,
+        photo: item.urlphoto,
+        docks: parseInt(item.attaches),
+        available_bikes: parseInt(item.bikes),
+        available_ebikes: parseInt(item.ebikes),
+        available_docks: parseInt(item.libres),
+        last_update: null,
+        dock_status: dock_status
+
+    };
 };
--- src/sources/bikepoint/velok.js
+++ src/sources/bikepoint/velok.js
@@ -1,7 +1,7 @@
 import request from 'request-promise-native';
 import xmlParser from '../../helpers/xmlParser';
 
-const getRaw = () => request('https://webservice.velok.lu/station.aspx');
+const getRaw = () => request('https://webservice.velok.lu/stationattache.aspx');
 
 export const get = async () => {
     var raw = await getRaw();
@@ -16,5 +16,55 @@ export const stations = async () => {
 };
 
 export const compileStation = item => {
-    return item;
+
+    var dock_status = [];
+    var attache, status, bikeType;
+
+    for (var i = 1; i <= item.attaches; i++) {
+
+        attache = parseInt(item['attache' + i]);
+
+        switch(attache) {
+        case 0:
+            status = 'free';
+            bikeType = null;
+            break;
+
+        case 1:
+            status = 'occupied';
+            bikeType = 'manual';
+            break;
+
+        case 2:
+            status = 'occupied';
+            bikeType = 'electric';
+            break;
+        }
+
+        dock_status.push({
+            status:   status,
+            bikeType: bikeType
+        });
+
+    }
+
+    return {
+        id:                 parseInt(item.nstation),
+        open:               item.active == 1,
+        name:               item.nom,
+        position: {
+            longitude:      parseFloat(item.latitude),
+            latitude:       parseFloat(item.longitude)
+        },
+        city:               item.nomlocalite,
+        address:            item.lieu,
+        photo:              item.urlphoto,
+        docks:              parseInt(item.attaches),
+        available_bikes:    parseInt(item.bikes),
+        available_ebikes:   parseInt(item.ebikes),
+        available_docks:    parseInt(item.libres),
+        last_update:        null,
+        dock_status:        dock_status
+
+    };
 };
