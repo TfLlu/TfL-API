63294344c63b305470daba366b9fb71c07517087
Thierry Degeling <thierry@syn2cat.lu>
2016-12-12 16:59:39 +0100

    cleanup

--- src/controller/bikepoint.js
+++ src/controller/bikepoint.js
@@ -1,13 +1,6 @@
-//import * as velok from '../source/bikepoint/velok';
-//import * as veloh from '../source/bikepoint/veloh';
 import * as bikepoint from '../service/bikepoint';
 
 export const index = async ctx => {
 
     ctx.body = await bikepoint.stations();
-
-    /*ctx.body = {
-        'velok': await velok.stations(),
-        'veloh': await veloh.stations()
-    };*/
 };
