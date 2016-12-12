31c9a46af71217f15c20be4d9a104421b015162e
Thierry Degeling <thierry@syn2cat.lu>
2016-12-12 11:58:21 +0100

    legacy code

--- dist/controller/bikepoint.js
+++ dist/controller/bikepoint.js
@@ -5,27 +5,13 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.index = undefined;
 
-var _requestPromiseNative = require('request-promise-native');
-
-var _requestPromiseNative2 = _interopRequireDefault(_requestPromiseNative);
-
-var _xml2js = require('xml2js');
-
 var _velok = require('../sources/bikepoint/velok');
 
 var velok = _interopRequireWildcard(_velok);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-var xmlOptions = {
-    async: true,
-    trim: true,
-    explicitArray: false
-};
 
 const index = exports.index = (() => {
     var _ref = _asyncToGenerator(function* (ctx) {
--- src/controller/bikepoint.js
+++ src/controller/bikepoint.js
@@ -1,12 +1,4 @@
-import request from 'request-promise-native';
-import {parseString} from 'xml2js';
 import * as velok from '../sources/bikepoint/velok';
-
-var xmlOptions = {
-    async: true,
-    trim: true,
-    explicitArray: false
-};
 
 export const index = async ctx => {
 
