226607b4160d0931d8c1d6db57b214ceaa6feb5d
Thierry Degeling <thierry@syn2cat.lu>
2016-12-22 23:26:40 +0100

    added parking occupancy from VDL

--- dist/controller/index.js
+++ dist/controller/index.js
@@ -12,6 +12,10 @@ var _bikepoint = require('./bikepoint');
 
 var bikepoint = _interopRequireWildcard(_bikepoint);
 
+var _occupancy = require('./occupancy');
+
+var occupancy = _interopRequireWildcard(_occupancy);
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-exports.default = { home, bikepoint };
+exports.default = { home, bikepoint, occupancy };
