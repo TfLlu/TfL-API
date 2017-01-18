916ccfff353328cae0372e752920ae1dd810d86f
Tezza <tezza@ion.lu>
2017-01-18 16:58:29 +0100

    sending data to influx (if present)

--- dist/monitor.js
+++ dist/monitor.js
@@ -1,12 +1,35 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+exports.middleware = exports.requestTime = undefined;
+
+var _config = require('./config');
+
+var _config2 = _interopRequireDefault(_config);
+
+var _influxdbNodejs = require('influxdb-nodejs');
+
+var _influxdbNodejs2 = _interopRequireDefault(_influxdbNodejs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-const onData = data => {};
+var influxdb = false;
+if ((0, _config2.default)('INFLUXDB')) {
+    influxdb = new _influxdbNodejs2.default((0, _config2.default)('INFLUXDB'));
+}
+
+const onData = data => {
+    if (!influxdb) return;
+    if (data.RESPONSE_TIME) {
+        influxdb.write('responses').field(data.RESPONSE_TIME).then();
+    } else if (data.REQUEST_TIME) {
+        influxdb.write('requests').field(data.REQUEST_TIME).then();
+    }
+};
 
 const routeAccess = router => {
     return (() => {
@@ -20,7 +43,7 @@ const routeAccess = router => {
 
             const data = {
                 path: layer.path,
-                time: responseTime
+                responseTime
             };
             ctx.monitor.ROUTE_ACCESS = data;
         });
@@ -43,9 +66,9 @@ const responseTime = () => {
                 url: ctx.request.url,
                 method: ctx.request.method,
                 status: ctx.response.status,
-                time: responseTime
+                responseTime
             };
-            console.log(`Response Time: ${ responseTime }ms`);
+            //console.log(`Response Time: ${responseTime}ms`);
         });
 
         return function (_x3, _x4) {
@@ -61,7 +84,7 @@ const requestTime = response => {
             url: response.config.url,
             method: response.config.method,
             status: response.status,
-            time: requestTime
+            requestTime
         }
     });
 };
