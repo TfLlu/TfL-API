9291e5f9baeee200cf584da0635a9a8b959cf38c
Tezza <tezza@ion.lu>
2017-02-03 11:03:07 +0100

    fix for single vel'Oh bikepoint

--- dist/source/bikepoint/veloh.js
+++ dist/source/bikepoint/veloh.js
@@ -9,7 +9,7 @@ var _requests = require('../../requests');
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-const loadBikePoints = exports.loadBikePoints = () => (0, _requests.veloh)();
+const loadBikePoints = exports.loadBikePoints = bikePoint => (0, _requests.veloh)(bikePoint);
 
 const all = exports.all = (() => {
     var _ref = _asyncToGenerator(function* () {
