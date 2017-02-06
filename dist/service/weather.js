398b39d495cdda11d20d0404cf53bdf125790be5
Tezza <tezza@ion.lu>
2017-02-06 15:14:57 +0100

    boom errors for weather

--- dist/service/weather.js
+++ dist/service/weather.js
@@ -9,14 +9,23 @@ var _meteolux = require('../source/weather/meteolux');
 
 var meteolux = _interopRequireWildcard(_meteolux);
 
+var _boom = require('boom');
+
+var _boom2 = _interopRequireDefault(_boom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 const current = exports.current = (() => {
     var _ref = _asyncToGenerator(function* () {
-        const current = yield meteolux.current();
-        return current;
+        try {
+            return yield meteolux.current();
+        } catch (err) {
+            throw new _boom2.default.serverUnavailable('The /weather endpoint is temporarily unavailable');
+        }
     });
 
     return function current() {
