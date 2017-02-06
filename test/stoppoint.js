5b1860bd86ea98e2f027c7c75f3174a68ad5f050
Tezza <tezza@ion.lu>
2017-02-06 15:27:28 +0100

    split departures tests from stoppoint

--- test/stoppoint.js
+++ test/stoppoint.js
@@ -51,65 +51,6 @@ describe('StopPoints', () => {
                 });
         });
     });
-    describe('GET /StopPoint/Departures/200405035', () => {
-        it('should get 10 departures from "Luxembourg, Gare Centrale"', (done) => {
-            chai.request(server)
-                .get('/StopPoint/Departures/200405035')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.should.be.json;
-                    res.body.should.be.a('array');
-                    res.body.length.should.be.equal(10);
-                    for(var i = 0; i < res.body.length; i++) {
-                        var departure = res.body[i];
-                        departure.type.should.be.a('String');
-                        if (departure.trainId !== null) {
-                            departure.trainId.should.be.a('String');
-                        }
-                        departure.line.should.be.a('String');
-                        departure.number.should.be.a('Number');
-                        departure.departure.should.be.a('Number');
-                        departure.delay.should.be.a('Number');
-                        departure.live.should.be.a('Boolean');
-                        departure.destination.should.be.a('String');
-                        if (departure.destinationId !== null) {
-                            departure.destinationId.should.be.a('Number');
-                        }
-                    }
-                    done();
-                });
-        });
-    });
-    describe('GET /StopPoint/Departures/200405035/'+randomLimit, () => {
-        it('should get a random number ['+randomLimit+'] of departures from "Luxembourg, Gare Centrale"', (done) => {
-            chai.request(server)
-                .get('/StopPoint/Departures/200405035/'+randomLimit)
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.should.be.json;
-                    res.body.should.be.a('array');
-                    res.body.length.should.be.equal(randomLimit);
-                    for(var i = 0; i < res.body.length; i++) {
-                        var departure = res.body[i];
-                        departure.type.should.be.a('String');
-                        if (departure.trainId !== null) {
-                            departure.trainId.should.be.a('String');
-                        }
-                        departure.line.should.be.a('String');
-                        departure.number.should.be.a('Number');
-                        departure.departure.should.be.a('Number');
-                        departure.departureISO.should.be.a('String');
-                        departure.delay.should.be.a('Number');
-                        departure.live.should.be.a('Boolean');
-                        departure.destination.should.be.a('String');
-                        if (departure.destinationId !== null) {
-                            departure.destinationId.should.be.a('Number');
-                        }
-                    }
-                    done();
-                });
-        });
-    });
     describe('GET /StopPoint/around/6.133646/49.60067/100', () => {
         it('should get stoppoints around Luxembourg Central station (LON:6.133646 LAT:49.60067 MAX_DISTANCE:100m) as geojson', (done) => {
             chai.request(server)
