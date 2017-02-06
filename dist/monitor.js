ebdb0aa6144c2dfd23fe9349053fbab44d9584d9
Tezza <tezza@ion.lu>
2017-02-06 16:29:02 +0100

    too much data

--- dist/monitor.js
+++ dist/monitor.js
@@ -30,7 +30,9 @@ const onData = data => {
         }
         influxdb.write('responses').field(data.RESPONSE_TIME).then();
     } else if (data.REQUEST_TIME) {
-        influxdb.write('requests').field(data.REQUEST_TIME).then();
+        /*influxdb.write('requests')
+            .field(data.REQUEST_TIME)
+            .then();*/
     }
 };
 
