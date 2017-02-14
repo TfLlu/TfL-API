23cb42951cfb3351a9b71219a8faf09d7c9ae344
Tezza <tezza@ion.lu>
2017-02-14 08:49:46 +0100

    semicolons

--- src/crawler/bikepoint.js
+++ src/crawler/bikepoint.js
@@ -34,7 +34,7 @@ const nextCrawl = () => {
         try {
             return crawl();
         } catch (err) {
-            console.log('BIKEPOINT CRAWLER ERROR', err.message)
+            console.log('BIKEPOINT CRAWLER ERROR', err.message);
         }
     });
 };
@@ -134,7 +134,7 @@ const crawl = async () => {
         CACHE_TTL
     );
 
-    nextCrawl()
+    nextCrawl();
 };
 
 // Run crawler
