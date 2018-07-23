d4eb3714c5db1b0966f62638844319048b89e2d1
Daniel Duton <daniel@duton.lu>
2018-07-23 14:39:25 +0200

    remove console.log

--- src/controller/weather.js
+++ src/controller/weather.js
@@ -14,7 +14,6 @@ export const current = async ctx => {
 };
 
 export const streamSingle = async ({ emit, disconnect }) => {
-    console.log('hellow');
     redis.incr(STREAM_CLIENTS_KEY);
     var res = weather.fireHose(data => {
         emit(data);
